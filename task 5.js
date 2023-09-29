d367184bfae09907394c931161c437b23e7823c1
Praveen kumar Muthiah <145551426+pkm0707@users.noreply.github.com>
2023-09-29 14:24:07 +0530

    Create task 5.js for JSON format resume
    kindly refer to the output also, which is mentioned in 55 line

---
+++ task 5.js
@@ -0,0 +1,55 @@
+let resume = {
+    "Basics":{
+        "Name":"Praveen Kumar M",
+        "DOB" :"07-07-2000",
+        "Address":"Theni",
+        "MobileNo":"9587463254",
+        "EmailId":"pkm007@gmail.com",
+    },
+    "CarrerObjective":"To work with passion for a leading organization which recognize employees    qualification and provide opportunities to learn and progress along with organization.",
+    "Academic Details":{
+        "PGdegree":"M.E Power System",
+        "PGdegree passed out":"2023",
+        "PGCollege":"TCE",
+        "PGCGPA":"9.21",
+        "UGdegree":"B.E EEE",
+        "UGdegree passed out":"2021",
+        "UGCollege":"MSEC",
+        "UGCGPA":"8.12",
+        "HSC":"BIO-MATHS",
+        "HSCCutt-off":"181.19",
+        "SSLCCut-off":"92.24", 
+        "School":"TMHNU"
+    },
+    "FieldofInterest":"Power System Analysis,Electric Vehicles,Renewable Energy (solar & wind )",
+    "ProjectDetails":{
+        "PGProject":"Optimisation of Electric vehicle scheduling with multiple vehicles in public transportation based on timetabled trips",
+        "UGProject":"Implementation of Selective Harmonic Elimination Technique for Multilevel Inverter using PUC Topology"
+    },
+    "IndustrialExposure":{
+        "IndustrialVisit":{
+            "a":"TVS tire manufacturing company, Madurai.",
+            "b":"Kaynes & LeePra Technologies, Electrical & Electronics Manufacturing Company, Mysore, Karnataka."
+        },
+        "InplantTraining":{
+            "1":"Bharat Sanchar Nigam Limited (BSNL) Telecommunication Technologies, Theni."
+        }
+    },
+    "WorkshopsAttended":{
+        "a":"Quantative aptitude Offered by Mepco Schlenk Engineering College, Sivakasi.",
+        "b":"Ethical Hacking Offered by CEG Guindy Campus, Anna University, Chennai."
+    },
+    "SkillsEarned":"C Programming,NI LabVIEW,MATLAB & Simulink,MS Office Packages",
+    "Certificates Earned":{
+        "a":"National Solar Vehicle Challenge (NSVC), conducted by DYNAMIST MOTORSPORTS in Visvesvaraya National Institute of Technology, Nagpur, Maharashtra",
+        "b":"Soft Skills and Personality Development conducted by NPTEL",
+        "c":"Completed Python programming Course in UDEMY"
+    },
+    "Hobbies":" Reading Books,Cycling, Playing Cricket, Carom & Chess",
+    "Languages known":"Tamil (speak & write),English (speak & write), Hindi (speak)",
+    "Declaration":"I hereby declare that the above furnished information is authentic to the best of my knowledge",
+    "Signature":"Praveen kumar M"
+}
+console.log(resume.IndustrialExposure.IndustrialVisit.a);
+
+o/p : TVS tire manufacturing company, Madurai.
